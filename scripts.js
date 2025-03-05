5da8f42033ea1ab84cbe2c020ac564e79958825d
mvlauriar <72217780+mvlauriar@users.noreply.github.com>
2025-03-05 13:32:13 -0500

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,6 @@
+// Wait for the DOM to be fully loaded
+document.addEventListener('DOMContentLoaded', function() {
+    console.log('Dashboard loading...');
+    
+    // We'll load the visualizations here
+});
